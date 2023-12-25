ffb7d532f51bf2af06f645a3cacccda89fe84298
Ehtisham Afzal <154811972+Ehtisham1053@users.noreply.github.com>
2023-12-25 18:18:03 +0500

    Add files via upload

---
+++ Cgpa.py
@@ -0,0 +1,311 @@
+def gpa_in_geology(num1):
+    if num1>=80:
+        
+        return ("\nyour gpa in geology is 4 and your grade i A+ ")
+    elif num1<=80 and num1>=76:
+        
+        return ("\nyour gpa in geology is 3.8 and your grade is A ")
+    elif num1 <=75 and num1>=72:
+        
+        return ("\nyour gpa in geology is 3.5 and your grade is B+ ")
+    elif num1<=71 and num1>=68:
+
+        return ("\nyour gpa in geology is 3  and your grade is B ")
+    elif num1 <=67 and num1>=64:
+        
+        return ("\nyour gpa in geology is 2.8 and your grade is B- ")
+    elif num1<=63 and num1>=60:
+        
+        return ("\nyour gpa in geology is 2.5 and your grage is C+ ")
+    elif num1<=59 and num1>=54:
+
+        return ("\nyour gpa in geology is 2  and your grade is C ")
+    elif num1<=53 and num1>=50:
+
+        print("\nyour gpa in geology is 1 and your grade is D ")
+    elif num1<=49 and num1>=1:
+
+        return ("\nyour gpa in geology is 1 and your garde is F")
+    else:
+        return ("thanks")
+    
+    
+def gpa_in_DB(num1):
+    if num1>=80:
+        
+        return ("\nyour gpa in DB is 4 and your grade i A+ ")
+    elif num1<=80 and num1>=76:
+        
+        return ("\nyour gpa in DB is 3.8 and your grade is A ")
+    elif num1 <=75 and num1>=72:
+        
+        return ("\nyour gpa in DB is 3.5 and your grade is B+ ")
+    elif num1<=71 and num1>=68:
+
+        return ("\nyour gpa in DB is 3  and your grade is B ")
+    elif num1 <=67 and num1>=64:
+        
+        return ("\nyour gpa in DB is 2.8 and your grade is B- ")
+    elif num1<=63 and num1>=60:
+        
+        return ("\nyour gpa in DB is 2.5 and your grage is C+ ")
+    elif num1<=59 and num1>=54:
+
+        return ("\nyour gpa in DB is 2  and your grade is C ")
+    elif num1<=53 and num1>=50:
+
+        print("\nyour gpa in DB is 1 and your grade is D ")
+    elif num1<=49 and num1>=1:
+
+        return ("\nyour gpa in DB is 1 and your garde is F")
+    else:
+        return ("thanks")
+    
+    
+    
+    
+def gpa_in_DS(num1):
+    if num1>=80:
+        
+        return ("\nyour gpa in DS is 4 and your grade i A+ ")
+    elif num1<=80 and num1>=76:
+        
+        return ("\nyour gpa in DS is 3.8 and your grade is A ")
+    elif num1 <=75 and num1>=72:
+        
+        return ("\nyour gpa in DS is 3.5 and your grade is B+ ")
+    elif num1<=71 and num1>=68:
+
+        return ("\nyour gpa in DS is 3  and your grade is B ")
+    elif num1 <=67 and num1>=64:
+        
+        return ("\nyour gpa in DS is 2.8 and your grade is B- ")
+    elif num1<=63 and num1>=60:
+        
+        return ("\nyour gpa in DS is 2.5 and your grage is C+ ")
+    elif num1<=59 and num1>=54:
+
+        return ("\nyour gpa in DS is 2  and your grade is C ")
+    elif num1<=53 and num1>=50:
+
+        print("\nyour gpa in DS is 1 and your grade is D ")
+    elif num1<=49 and num1>=1:
+
+        return ("\nyour gpa in DS is 1 and your garde is F")
+    else:
+        return ("thanks")
+    
+    
+    
+def gpa_in_CA(num1):
+    if num1>=80:
+        
+        return ("\nyour gpa in CA is 4 and your grade i A+ ")
+    elif num1<=80 and num1>=76:
+        
+        return ("\nyour gpa in CA is 3.8 and your grade is A ")
+    elif num1 <=75 and num1>=72:
+        
+        return ("\nyour gpa in CA is 3.5 and your grade is B+ ")
+    elif num1<=71 and num1>=68:
+
+        return ("\nyour gpa in CA is 3  and your grade is B ")
+    elif num1 <=67 and num1>=64:
+        
+        return ("\nyour gpa in CA is 2.8 and your grade is B- ")
+    elif num1<=63 and num1>=60:
+        
+        return ("\nyour gpa in CA is 2.5 and your grage is C+ ")
+    elif num1<=59 and num1>=54:
+
+        return ("\nyour gpa in CA is 2  and your grade is C ")
+    elif num1<=53 and num1>=50:
+
+        return ("\nyour gpa in CA is 1 and your grade is D ")
+    elif num1<=49 and num1>=1:
+
+        return ("\nyour gpa in CA is 1 and your garde is F")
+    else:
+        return ("thanks")
+    
+    
+def gpa_in_SAD(num1):
+    if num1>=80:
+        
+        return ("\nyour gpa SAD is 4 and your grade i A+ ")
+    elif num1<=80 and num1>=76:
+        
+        return ("\nyour gpa in  SAD is 3.8 and your grade is A ")
+    elif num1 <=75 and num1>=72:
+        
+        return ("\nyour gpa in  SAD is 3.5 and your grade is B+ ")
+    elif num1<=71 and num1>=68:
+
+        return ("\nyour gpa in  SAD is 3  and your grade is B ")
+    elif num1 <=67 and num1>=64:
+        
+        return ("\nyour gpa in  SAD is 2.8 and your grade is B- ")
+    elif num1<=63 and num1>=60:
+        
+        return ("\nyour gpa in  SAD is 2.5 and your grage is C+ ")
+    elif num1<=59 and num1>=54:
+
+        return ("\nyour gpa in  SAD is 2  and your grade is C ")
+    elif num1<=53 and num1>=50:
+
+        print("\nyour gpa in  SAD is 1 and your grade is D ")
+    elif num1<=49 and num1>=1:
+
+        return ("\nyour gpa in  SAD is 1 and your garde is F")
+    else:
+        return ("thanks")
+    
+    
+    
+def gpa_in_LinearMATH(num1):
+    if num1>=80:
+        
+        return ("\nyour gpa in linear math is 4 and your grade i A+ ")
+    elif num1<=80 and num1>=76:
+        
+        return ("\nyour gpa in linear math is 3.8 and your grade is A ")
+    elif num1 <=75 and num1>=72:
+        
+        return ("\nyour gpa in linear math is 3.5 and your grade is B+ ")
+    elif num1<=71 and num1>=68:
+
+        return ("\nyour gpa in linear math is 3  and your grade is B ")
+    elif num1 <=67 and num1>=64:
+        
+        return ("\nyour gpa in linear math is 2.8 and your grade is B- ")
+    elif num1<=63 and num1>=60:
+        
+        return ("\nyour gpa in linear math is 2.5 and your grage is C+ ")
+    elif num1<=59 and num1>=54:
+
+        return ("\nyour gpa in linear math is 2  and your grade is C ")
+    elif num1<=53 and num1>=50:
+
+        print("\nyour gpa in linear math is 1 and your grade is D ")
+    elif num1<=49 and num1>=1:
+
+        return ("\nyour gpa in linear math is 1 and your garde is F")
+    else:
+        return ("thanks")
+        
+
+mark_geology=int(input("enter your marks in geology\n"))
+mark_DB=int(input("enter your marks in DB\n"))
+mark_DS=int(input("enter your marks in DS\n"))
+mark_SAD=int(input("enter your marks in SAD\n"))
+mark_CA=int(input("enter your marks in CA\n"))
+mark_linearMATH=int(input("enter your marks in linear math\n"))
+
+geo=gpa_in_geology(mark_geology)
+print(geo)
+
+DB=gpa_in_DB(mark_DB)
+print(DB)
+
+DS=gpa_in_DS(mark_DS)
+print(DS)
+
+SAD=gpa_in_SAD(mark_SAD)
+print(SAD)
+
+
+CA=gpa_in_CA(mark_CA)
+print(CA)
+
+LinearMATH=gpa_in_LinearMATH(mark_linearMATH)
+print(LinearMATH)
+
+
+
+
+
+
+with open("new.txt","w")as f:
+    f.write("MR, Ehtisham your semester result is as follow")
+
+with open ("new.txt","a") as f:
+    f.write(geo)
+ 
+with open ("new.txt","a") as f:
+    f.write(DB) 
+ 
+with open ("new.txt","a") as f:
+    f.write(DS) 
+  
+with open ("new.txt","a") as f:
+    f.write(SAD)
+  
+with open ("new.txt","a") as f:
+    f.write(CA)
+    
+with open ("new.txt","a") as f:
+    f.write(LinearMATH) 
+    
+    
+c1=input("enter your gpa in geology  ")
+c2=input("enter your gpa in DB  ")
+c3=input("enter your gpa in DS ")
+c4=input("enter your gpa in SAD ")
+c5=input("enter your gpa in CA ")
+c6=input("enter your gpa in Linear math ")
+print(f"the value of C1 is {c1},,the valus of C2 is {c2},,the value of C3 is {c3},,")
+print(f"the value of C4 is {c4},,the valus of C5 is {c5},,the value of C6 is {c6},,")
+
+
+print("operation 1=int concersion, operation 2=float conversion\n")
+C1operation=int(input("enter the opeartion valuse for C1: "))
+C2operation=int(input("enter the opeartion valuse for C2: "))
+C3operation=int(input("enter the opeartion valuse for C3: "))
+C4operation=int(input("enter the opeartion valuse for C4: "))
+C5operation=int(input("enter the opeartion valuse for C5: "))
+C6operation=int(input("enter the opeartion valuse for C6: "))
+
+if (C1operation)==1:
+    c1=int(c1) 
+elif C1operation==2:
+    c1=float(c1)
+    
+if C2operation==1:
+    c2=int(c2)
+elif C2operation==2:
+    c2=float(c2)
+
+if C3operation==1:
+    c3=int(c3)
+elif C3operation==2:
+    c3=float(c3)
+
+if C4operation==1:
+    c4=int(c4)
+elif C4operation==2:
+    c4=float(c4)
+
+if C5operation==1:
+    c5=int(c5)
+elif C5operation==2:
+    c5=float(c5)
+
+if C6operation==1:
+    c6=int(c6)
+elif C6operation==2:
+    c6=float(c6)
+
+list=[c1,c2,c3,c4,c5,c6]
+print(list)
+
+CGPA=sum(list)/6
+print(CGPA)
+
+CGPA=str(CGPA)
+note="\n\nhey Mr, Ehtisham your CGPA is:  "
+with open ("new.txt","a")as f:
+    f.write(note)
+    
+with open("new.txt","a")as f:
+    f.write(CGPA)
+    
